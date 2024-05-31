de0b598d5307e66a556635e2f16a08d3517dd74c
Alex Hewitt <alexhewitt.j@gmail.com>
2024-05-31 14:24:47 -0500

    add tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -14,6 +14,26 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This  is a test", "bold")
         self.assertNotEqual(node, node2)
 
+    def test_no_url(self):
+        node = TextNode("This is a test", "bold", None)
+        node2 = TextNode("This is a test", "bold")
+        self.assertEqual(node, node2)
+
+    def test_different_text_type(self):
+        node = TextNode("This is a test", "italic")
+        node2 = TextNode("This is a test", "bold")
+        self.assertNotEqual(node, node2)
+
+    def test_different_urls(self):
+        node = TextNode("This is a test", "bold", "test.com")
+        node2 = TextNode("This is a test", "bold", "tst.com")
+        self.assertNotEqual(node, node2)
+
+    def test_same_urls(self):
+        node = TextNode("This is a test", "bold", "test.com")
+        node2 = TextNode("This is a test", "bold", "test.com")
+        self.assertEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
