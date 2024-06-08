94e4e059f3b57fc57bf8c0e4580f7d0029ed22f4
Alex Hewitt <alexhewitt.j@gmail.com>
2024-06-07 20:59:30 -0500

    add inline markdown and tests

---
+++ src/inline_markdown.py
@@ -0,0 +1,31 @@
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+)
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        temp = []
+        if type(node) is TextNode:
+            split = node.text.split(delimiter) 
+            length = len(split)
+            for i in range(length):
+                if split[i] == "":
+                    continue
+                elif length % 2 == 0:
+                    raise ValueError("Closing/opening delimiter not found.")
+                elif i % 2 != 0:
+                    temp.append(TextNode(split[i], text_type))
+                else:
+                    # Back to back delimiters
+                    temp.append(TextNode(split[i], text_type_text))
+        else:
+            new_nodes.append(node)
+        new_nodes.extend(temp)
+
+        return new_nodes
+
--- src/main.py
+++ src/main.py
@@ -1,24 +1,28 @@
+from inline_markdown import split_nodes_delimiter
 from textnode import TextNode
 from htmlnode import HTMLNode, ParentNode, LeafNode
 
 
 def main():
-    print(TextNode('This is a text node', 'bold', 'https://www.boot.dev'))
-    test = HTMLNode('a', 'none', [], {
-                    'href': 'https://www.google.com', 'target': 'blank'})
-    test.props_to_html()
-    print(LeafNode('a', 'Link here', {'href': 'www.test.com'}).to_html())
-    node = ParentNode(
-    "p",
-    [
-        LeafNode("b", "Bold text"),
-        LeafNode(None, "Normal text"),
-        LeafNode("i", "italic text"),
-        LeafNode(None, "Normal text"),
-    ],
-    )
-    print(node)
-    print(node.to_html())
+    # print(TextNode('This is a text node', 'bold', 'https://www.boot.dev'))
+    # test = HTMLNode('a', 'none', [], {
+    #                 'href': 'https://www.google.com', 'target': 'blank'})
+    # test.props_to_html()
+    # print(LeafNode('a', 'Link here', {'href': 'www.test.com'}).to_html())
+    # node = ParentNode(
+    # "p",
+    # [
+    #     LeafNode("b", "Bold text"),
+    #     LeafNode(None, "Normal text"),
+    #     LeafNode("i", "italic text"),
+    #     LeafNode(None, "Normal text"),
+    # ],
+    # )
+    # print(node)
+    # print(node.to_html())
+    node = TextNode("This is text with a `code block``ligma` word", "text")
+    new_node = split_nodes_delimiter([node], "`", "code")
+    print(new_node)
 
 
 if __name__ == "__main__":
