4f0567a6e270a4a515ee62c0fb216a239182c327
Alex Hewitt <alexhewitt.j@gmail.com>
2024-06-20 20:39:07 -0500

    solution

--- src/generate.py
+++ src/generate.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
@@ -30,7 +31,16 @@ def generate_page(from_path, template_path, dest_path):
 
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    dirs = os.listdir(dir_path_content)
-    print(dirs)
-    for dir in dirs:
-        generate_page(f"content/{dir}", template_path, f"{dest_dir_path}/{dir[:-3]}.md")
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        if os.path.isfile(from_path):
+            if Path(from_path).suffix == ".md":
+                dest_path = os.path.join(dest_dir_path, f"{Path(from_path).stem}.html")
+                generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(
+                from_path, template_path, os.path.join(dest_dir_path, filename)
+            )
